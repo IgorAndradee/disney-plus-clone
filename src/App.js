8434fe58f614c5b6375f3b6486d0a80232d411ec
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-17 22:41:03 -0300

    Create Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
