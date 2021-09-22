3f4277185e00994539f439c03590cb18795f2523
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-22 00:18:54 -0300

    Add React Slick Carousel

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,7 +1,47 @@
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ImgSlider = (props) => {
-  return <div>Image Slider</div>;
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+    </Carousel>
+  );
 };
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+`;
 
 export default ImgSlider;
