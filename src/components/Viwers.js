d20a0b2cc050777a8c349d3320ef1502aeef288e
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-24 13:31:13 -0300

    Add video to Viwers

--- src/components/Viwers.js
+++ src/components/Viwers.js
@@ -1,22 +1,40 @@
 import styled from "styled-components";
 
-const Viwers = (props) => {
+const Viewers = (props) => {
   return (
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
       </Wrap>
     </Container>
   );
@@ -44,7 +62,6 @@ const Wrap = styled.div`
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-
   img {
     inset: 0px;
     display: block;
@@ -57,6 +74,23 @@ const Wrap = styled.div`
     z-index: 1;
     top: 0;
   }
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    video {
+      opacity: 1;
+    }
+  }
 `;
 
-export default Viwers;
+export default Viewers;
